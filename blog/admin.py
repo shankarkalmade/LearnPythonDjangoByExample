dd45539f30985775d2f1354f056dce4800e363f2
shankar.kalmade <shankar.kalmade@gmail.com>
2017-09-26 16:31:41 +0530

    Chapter first working

---
+++ blog/admin.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from .models import Post
+# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish', 'status')
+    search_fields = ('title', 'body')
+    list_filter = ('status', 'created', 'publish', 'author')
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
+
+admin.site.register(Post, PostAdmin)
